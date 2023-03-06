80127f788d89a216d1da8b21c4a8d519ac05661c
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-03-07 00:00:19 +0300

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+ language that combines remarkable power with very clear syntax"
+
+# find the index of the substring "object-oriented programming with Python"
+index = str.find("object-oriented programming with Python")
+
+# extract the substring and print it
+substring = str[index:index+len("object-oriented programming with Python")]
+print(substring + "\n")
