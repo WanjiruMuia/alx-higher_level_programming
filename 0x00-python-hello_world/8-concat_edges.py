fec6a4d32336dba67a7f0a4e5b5227f55e54f1e1
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-03-06 23:49:46 +0300

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+str[index:index+len("object-oriented programming with Python")]
+print(substring + "\n")
 
