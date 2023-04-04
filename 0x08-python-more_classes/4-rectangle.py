aeea9db358b3f13cff288f6533b1fefdd0236424
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-04 15:23:10 +0300

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
 """Defines a Rectangle class."""
-class rectangle:
+
+
+class Rectangle:
     """Represent a rectangle."""
-     def __init__(self, width=0, height=0):
+
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
