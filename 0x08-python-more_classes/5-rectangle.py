01abfe8c51498383b37be963bab11350f59b78a6
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-04 15:10:32 +0300

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -2,11 +2,19 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+        def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get/set the width of the Rectangle."""
         return self._width
 
     @width.setter
@@ -20,6 +28,7 @@ class Rectangle:
 
     @property
     def height(self):
+         """Get/set the height of the Rectangle."""
         return self._height
 
     @height.setter
@@ -32,22 +41,30 @@ class Rectangle:
             self._height = value
 
     def area(self):
+        """Return the area of the Rectangle."""
         return self.width * self.height
 
     def perimeter(self):
+         """Return the perimeter of the Rectangle."""
         if self.width == 0 or self.height == 0:
             return 0
         else:
             return 2 * (self.width + self.height)
 
     def __str__(self):
+        """Return the printable representation of the Rectangle.
+        
+        Represents the rectangle with the # character.
+        """
         if self.width == 0 or self.height == 0:
             return ''
         else:
             return '\n'.join(['#' * self.width] * self.height)
 
     def __repr__(self):
+        """Return the string representation of the Rectangle."""
         return f'Rectangle({self.width}, {self.height})'
 
     def __del__(self):
+         """Print a message for every deletion of a Rectangle."""
         print("Bye rectangle...")
