6880af395fb2b67014ed9ce09750f5cb8aac2c73
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-12 19:17:01 +0300

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,23 +1,15 @@
-!/usr/bin/python3
-# 14-pascal_triangle.p
-"""Defines a Pascal's Triangle function."""
-
-
+#!/usr/bin/python3
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
-    Returns a list of lists of integers representing the triangle.
-    """
+    """ Pascal Triangle 1h"""
+    li = []
     if n <= 0:
-        return []
-
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return 
-  
-  
+        return li
+    for r in range(n):
+        for c in range(r + 1):
+            if c == 0:
+                li.append([1])
+            elif c == r:
+                li[r].append(1)
+            else:
+                li[r].append(li[r - 1][c] + li[r - 1][c - 1])
+    return li
