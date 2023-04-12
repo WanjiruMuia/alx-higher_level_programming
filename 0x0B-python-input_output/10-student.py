8b4719fb023a9474fa1f8b148075eb97bf30bf43
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-12 18:45:49 +0300

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Defines a class Student."""
 class Student:
+    """Represent a student."""
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -13,4 +16,5 @@ class Student:
             for key in attrs:
                 if hasattr(self, key):
                     filtered_dict[key] = getattr(self, key)
+                    """Get a dictionary representation of the Student."""
             return filtered_dict
