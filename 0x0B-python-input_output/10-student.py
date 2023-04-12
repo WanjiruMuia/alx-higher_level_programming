ac0e40dd5a7e60d8391e076c480eaa242474de57
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-12 18:43:29 +0300

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-Student = __import__('10-student').Student
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
-
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
-
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    filtered_dict[key] = getattr(self, key)
+            return filtered_dict
