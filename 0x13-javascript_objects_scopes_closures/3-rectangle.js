baf931ef471e15c47565dda5f053cb69f6877bf8
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-13 22:24:26 +0300

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 
-const rectangle1 = new Rectangle(5, 4);
-rectangle1.print();
-// Output:
-// XXXXX
-// XXXXX
-// XXXXX
-// XXXXX
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-const rectangle2 = new Rectangle(0, 5);
-rectangle2.print();
-// Output:
-// Empty object
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
 
-const rectangle3 = new Rectangle(3.5, 2);
-rectangle3.print();
-// Output:
-// Empty object
+module.exports = Rectangle;
