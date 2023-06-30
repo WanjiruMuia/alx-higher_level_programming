d63fc1b80e70939de1a3103aed413563e77262a4
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-30 22:38:53 +0300

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a search request for a given string to the Star Wars API.
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {"q": argv[1] if len(argv) > 1 else ""}
+    request = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        json = request.json()
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
