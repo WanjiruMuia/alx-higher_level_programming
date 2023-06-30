c55d700341ffee5d6c5a0f7042a0d3bcda2ba084
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-30 22:51:56 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/python3
-
-url="$1"
-
-response=$(curl -sI "$url")
-content_length=$(echo "$response" | awk '/Content-Length/ {print $2}' | tr -d '\r')
-
-if [ -n "$content_length" ]; then
-    echo "Response body size: ${content_length} bytes"
-else
-    echo "Unable to determine response body size"
-fi
+#!/bin/bash
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
