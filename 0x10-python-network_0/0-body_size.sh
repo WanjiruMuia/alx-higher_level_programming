5fb7acb40fca2f52511a453ff9350616e204863d
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-30 22:50:48 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
-# Displays the size of the body of the response of a curl request
-curl -so /dev/null -w '%{size_download}\n' "$1"
+#!/usr/bin/python3
+
+url="$1"
+
+response=$(curl -sI "$url")
+content_length=$(echo "$response" | awk '/Content-Length/ {print $2}' | tr -d '\r')
+
+if [ -n "$content_length" ]; then
+    echo "Response body size: ${content_length} bytes"
+else
+    echo "Unable to determine response body size"
+fi
