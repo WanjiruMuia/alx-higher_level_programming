e981531eee1c62a7b7f7f6650e5f18eb9f733ca7
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-06-30 22:37:49 +0300

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    request = requests.get(argv[1])
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
+    else:
+        print(request.text)
