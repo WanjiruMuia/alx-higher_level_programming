e6a8fa52235cc91d9133f6a44a1f417ae0064440
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-25 20:52:39 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,11 @@
-import sys
+#!/usr/bin/node
 
-def read_and_print_file_content(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except Exception as e:
-        print(f"An error occurred while reading the file: {e}")
+const fs = require('fs');
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <file_path>")
-    else:
-        file_path = sys.argv[1]
-        read_and_print_file_content(file_path)
-
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
