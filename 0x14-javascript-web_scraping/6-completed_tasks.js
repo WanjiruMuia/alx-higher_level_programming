7efd3b398bdfdf0bb1696d064d15278298024760
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-25 21:16:00 +0300

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,29 +1,25 @@
 #!/usr/bin/node
-import requests
 
-def get_completed_tasks_by_user(api_url):
-    try:
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            tasks = response.json()
+const request = require('request');
 
-            # Create a dictionary to store the count of completed tasks for each user
-            user_completed_tasks = {}
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
 
-            # Iterate through the tasks and count completed tasks for each user
-            for task in tasks:
-                if task['completed']:
-                    user_id = task['userId']
-                    user_completed_tasks[user_id] = user_completed_tasks.get(user_id, 0) + 1
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
 
-            # Print users with completed tasks and the number of completed tasks for each user
-            for user_id, completed_tasks in user_completed_tasks.items():
-                print(f"User ID: {user_id}, Completed Tasks: {completed_tasks}")
-        else:
-            print(f"Error: Unable to fetch data from the API. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
 
-if __name__ == "__main__":
-    api_url = "https://jsonplaceholder.typicode.com/todos"
-    get_completed_tasks_by_user(api_url)
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
+});
