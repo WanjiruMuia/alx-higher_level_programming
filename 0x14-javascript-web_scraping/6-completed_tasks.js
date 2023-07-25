322fb497208d1392121612afd63f1758b7065e01
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-07-25 21:12:46 +0300

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-
-const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
 });
