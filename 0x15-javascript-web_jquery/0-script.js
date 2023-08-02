d74cefc6a261ee7d4701eeae223fa3d598f00abc
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-08-02 12:16:19 +0300

    0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Holberton School</title>
-    <link rel="stylesheet" href="styles/global.css" />
-</head>
-<body>
-    <header> 
-        First HTML page
-    </header>
-    <section>
-        <img src="logo.jpg" alt="" />
-        <br />
-        <ul>
-            <li>Home</li>
-            <li>Admission</li>
-            <li>Login</li>
-        </ul>
-    </section>
-    <footer>
-        Holberton School - 2017
-    </footer>
+// Javascript script that updates the text color of the <header> element to red (#FF0000)
 
-    <script>
-        // Select the <header> element using document.querySelector
-        const headerElement = document.querySelector('header');
-
-        // Update the text color to red (#FF0000)
-        headerElement.style.color = '#FF0000';
-    </script>
-</body>
-</html>
-
+const colorH = document.querySelector('header');
+colorH.style.color = '#FF0000';  
