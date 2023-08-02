c7c2562e6af3648a6a570cf3da1089279d33aec9
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-08-02 12:21:27 +0300

    2-script.js

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Holberton School</title>
-    <link rel="stylesheet" href="styles/global.css" />
-    <!-- Include jQuery from a CDN -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
-    <header> 
-        First HTML page
-    </header>
-    <section>
-        <img src="logo.jpg" alt="" />
-        <br />
-        <ul>
-            <li>Home</li>
-            <li>Admission</li>
-            <li>Login</li>
-        </ul>
-    </section>
-    <footer>
-        Holberton School - 2017
-    </footer>
+// JavaScript script that updates the text color of the <header> element to red (#FF0000) 
+// when the user clicks on the tag DIV#red_header
 
-    <script>
-        // Use the jQuery API to update the text color of the <header> element to red
-        $(document).ready(function () {
-            $('header').css('color', '#FF0000');
-        });
-    </script>
-</body>
-</html>
-
+$('DIV#red_header').click(function () {
+    $('header').css({ color: '#FF0000' });
+});      
