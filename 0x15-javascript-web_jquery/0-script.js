a18fdab964e32395106ad7fcf83395824913f660
WanjiruMuia <mercymuia5@gmail.com>
2023-08-02 02:00:58 -0700

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Holberton School</title>
+    <link rel="stylesheet" href="styles/global.css" />
+</head>
+<body>
+    <header> 
+        First HTML page
+    </header>
+    <section>
+        <img src="logo.jpg" alt="" />
+        <br />
+        <ul>
+            <li>Home</li>
+            <li>Admission</li>
+            <li>Login</li>
+        </ul>
+    </section>
+    <footer>
+        Holberton School - 2017
+    </footer>
+
+    <script>
+        // Select the <header> element using document.querySelector
+        const headerElement = document.querySelector('header');
+
+        // Update the text color to red (#FF0000)
+        headerElement.style.color = '#FF0000';
+    </script>
+</body>
+</html>
+
